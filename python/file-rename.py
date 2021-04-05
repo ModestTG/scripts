47bd6b6aa7050875957970d83487cfbb1ca7f054
Elliot Weishaar <elliotweishaar27@gmail.com>
2021-04-05 15:59:16 -0500

    Added exiting file handling

--- python/file-rename.py
+++ python/file-rename.py
@@ -5,11 +5,26 @@ import sys
 
 prefix = sys.argv[1]
 path = sys.argv[2]
+duplicate = False
 
-for i, filename in enumerate(os.listdir(path)):
-    dst = f"{prefix}{(i + 1):04}{filename[-4:]}"
-    src = f"{path}/{filename}"
-    #src = "1920x1080/" + filename
-    dst = f"{path}/{dst}"
-    #dst = "1920x1080/" + dst
-    os.rename(src,dst)
+def main():
+    rename(prefix, path)
+    if duplicate:
+        rename(path, prefix)
+
+def rename(prefix, path):
+    for i, filename in enumerate(os.listdir(path)):
+        dst = f"{prefix}{(i + 1):04}{filename[-4:]}"
+        src = f"{path}/{filename}"
+        #src = "1920x1080/" + filename
+        dst = f"{path}/{dst}"
+        #dst = "1920x1080/" + dst
+        try:
+            os.rename(src,dst)
+        except FileExistsError:
+            duplicate = True
+            continue
+
+
+if __name__ == '__main__':
+    main()
