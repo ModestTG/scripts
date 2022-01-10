2460174f72ef89f4ae134c8bdd50f7b28024444a
Elliot Weishaar <elliotweishaar27@gmail.com>
2022-01-09 22:40:12 -0600

    Updated curl position

--- bash/container_monthly_backup.sh
+++ bash/container_monthly_backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ssh docker@docker.lan.ewhomelab.com 'for i in $(ls /home/docker/container_data); do mkdir -p /mnt/backup/$i/monthly && tar czvf /mnt/backup/$i/monthly/$i-$(date "+%Y%m%d").tar.gz /home/docker/container_data/$i; done'
-curl http://docker.lan.ewhomelab.com:8000/ping/f30feea2-632c-4da0-b5ab-c2425b8e5f04
+ssh docker@docker.lan.ewhomelab.com 'for i in $(ls /home/docker/container_data); do mkdir -p /mnt/backup/$i/monthly && tar czvf /mnt/backup/$i/monthly/$i-$(date "+%Y%m%d").tar.gz /home/docker/container_data/$i; done && curl http://docker.lan.ewhomelab.com:8000/ping/f30feea2-632c-4da0-b5ab-c2425b8e5f04'
 find /mnt/backup/**/monthly/* -mtime + -delete
 find /mnt/backup/* -empty -type d -delete
